b859bc93792834c89548d6bb2af6f8460c2750f2
mygit <ralozoms@gmail.com>
2022-03-23 23:34:56 +0100

    Update all_categories.py

--- examples/all_categories.py
+++ examples/all_categories.py
@@ -1,7 +1,13 @@
 import techspecs
-# All Categories
 
-techspecs_key = ""
-techspecs_base = ""
-categories = techspecs.categories(techspecs_base, techspecs_key, mode='pretty')
+# TechSpecs API Key
+techspecs_key = "techspecs_api_key" 
+
+# TechSpecs base https://apis.dashboard.techspecs.io/{techspecs_base}
+techspecs_base = "a8TD3mkN49fhg2y"    
+
+# choose between "pretty" or "raw" mode for viewing response
+categories = techspecs.categories(techspecs_base, techspecs_key, mode='pretty') 
+
+# print the search results
 print(categories)
