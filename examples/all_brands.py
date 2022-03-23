a96a5033c8ec6ec972740a07362ece06146bb209
mygit <ralozoms@gmail.com>
2022-03-23 23:34:35 +0100

    Update all_brands.py

--- examples/all_brands.py
+++ examples/all_brands.py
@@ -1,7 +1,14 @@
 import techspecs
-# All brands
 
-techspecs_key = ""
-techspecs_base = ""
-brands = techspecs.brands(techspecs_base, techspecs_key, mode='pretty')
+# TechSpecs API Key
+techspecs_key = "techspecs_api_key"          
+
+# TechSpecs base https://apis.dashboard.techspecs.io/{techspecs_base}
+techspecs_base = "a8TD3mkN49fhg2y"         
+
+# choose between "pretty" or "raw" mode for viewing response
+brands = techspecs.brands(techspecs_base, techspecs_key, mode='pretty') 
+
+# print the search results
 print(brands)
+
