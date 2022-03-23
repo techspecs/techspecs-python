60fbd96b5e9b62e117383cf9b49e0a1fd58b2583
mygit <ralozoms@gmail.com>
2022-03-23 23:33:20 +0100

    Update search.py

--- examples/search.py
+++ examples/search.py
@@ -1,11 +1,19 @@
 import techspecs
-# Search Product
+import json
 
-techspecs_key = ""
-techspecs_base = ""
+# TechSpecs API Key
+key = "techspecs_api_key"   
+
+# TechSpecs base https://apis.dashboard.techspecs.io/{techspecs_base}
+base = 'a8TD3mkN49fhg2y'     
+
 query = {
-    'keyword': '',
-    'category': '',
+    'keyword': 'iPhone 13', # product name or version number to search 
+    'category': 'all',      # product category to search
 }
-search = techspecs.search(techspecs_base, query, techspecs_key, mode='pretty')
+
+# choose between "pretty" or "raw" mode for viewing response
+search = techspecs.search(base, query, key, mode='pretty') 
+
+# print the search results
 print(search)
