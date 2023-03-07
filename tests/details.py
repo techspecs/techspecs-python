474be0f1ae9afe69bc8b5720e5dd9e3c4c6a2cf4
TechSpecs <ralozoms@gmail.com>
2023-03-07 12:47:31 +0100

    Delete details.py

--- tests/details.py
+++
@@ -1,9 +0,0 @@
-import techspecs
-# Product Details
-
-techspecs_key = ""
-techspecs_base = ""
-techspecs_id = ""
-
-details = techspecs.detail(techspecs_base, techspecs_id, techspecs_key, mode='pretty')
-print(details)
